06d641360c5e3c0b1ed06dfcd271a5d6f6fe19ae
Jacob Maurer <jacob.maurer.24@cnu.edu>
2025-06-02 15:52:25 -0400

    renamed Model Utils and created a file for generating neural weight data

---
+++ AI_weight_generation.py
@@ -0,0 +1,7 @@
+from model_utils import save_network, create_layers, model_string_generator, train, test
+
+def run_tests(x_set, y_set, num_test):
+    
+
+if __name__ == "__main__":
+    print("Hello world")
