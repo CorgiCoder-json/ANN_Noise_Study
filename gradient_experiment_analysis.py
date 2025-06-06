4a419b0a36e27794becd0b796ea176a5a9762a8a
Jacob Maurer <jacob.maurer.24@cnu.edu>
2025-06-06 18:40:31 -0400

    changes to testing files to run a new test; results indicate that the one_pass + gradient did not improve accuracy

--- gradient_experiment_analysis.py
+++ gradient_experiment_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
 
 def open_logs(fpath):
     round_results = []
@@ -19,7 +20,7 @@ def open_logs(fpath):
                 
 
 if __name__ == "__main__":
-    logs = open_logs("./regression/reports/gradient_experimental_logs.txt")
+    logs = open_logs("./regression/reports/gradient_experimental_logs_test_1.txt")
     print(len(logs))
     imperfection_tracker = 0
     wierd_tracker = 0
@@ -35,11 +36,14 @@ if __name__ == "__main__":
             continue
     print(f"There are {imperfection_tracker} imperfections, which means that in {((len(logs)-imperfection_tracker)/len(logs)) * 100}% of cases, there is improvement")
     print(f"There are {wierd_tracker} wierd results, and {no_takeover_tracker} no improvement results.")
-    loss_stats = pd.read_csv("D:\\pass_gradient_exp\\stats.csv")
+    loss_stats = pd.read_csv("D:\\pass_gradient_exp\\regression_test_1\\stats.csv")
     print(loss_stats)
     for i in range(len(logs)):
-        if logs[i] == 1 or logs[i] == 2:
+        if logs[i] == 2:
             loss_stats.drop(i, axis=0, inplace=True)
+    temp_percent = loss_stats["percent"]
+    temp_percent *= np.sign(loss_stats["differences"].to_numpy())
+    loss_stats["percent"] = temp_percent
     print(loss_stats)
     print(loss_stats["percent"].to_numpy())
     sns.histplot(loss_stats, x="percent", kde=True).set_title("Percent Improvement Distribution")
--- one_pass_plus_graident.py
+++ one_pass_plus_graident.py
@@ -24,7 +24,7 @@ def get_percent_imporvement(start_loss, min_loss):
     return (math.fabs(start_loss - min_loss) / start_loss) * 100.0
 
 if __name__ == "__main__":
-    log_path = "regression/reports/gradient_experimental_logs_test_2.txt"
+    log_path = "regression/reports/gradient_experimental_logs_test_3.txt"
     with open(log_path, 'wt') as file:
         dataset = pd.read_csv("generated_data_sets/small_5000_100_10_regression_generated.csv")
         x_vals = dataset[dataset.columns[dataset.columns != 'y']].to_numpy()
@@ -38,7 +38,7 @@ if __name__ == "__main__":
         trained_copy_loss = []
         losses = []
         loss_fn = nn.MSELoss()
-        model_string = '100|200->silu->200|150->relu->150|1'
+        model_string = '100|200->silu->200|150->silu->150|1'
         for j in range(32):
             temp_model = NetworkSkeleton(create_layers(model_string, {'relu': nn.ReLU(), 'silu': nn.SiLU()}))
             model_copy = copy.deepcopy(temp_model)
@@ -48,12 +48,12 @@ if __name__ == "__main__":
             min_acc = np.inf
             trained_rounds = 0
             minimum_model: NetworkSkeleton = NetworkSkeleton([])
-            save_model_parameters(temp_model, model_string, f'base_round_{j}', 'D:\\pass_gradient_exp\\regression_test_2', global_device)
+            save_model_parameters(temp_model, model_string, f'base_round_{j}', 'D:\\pass_gradient_exp\\regression_test_3', global_device)
             for i in range(8):
                 file.write(f"MSE OF THE TRAINED MODEL AFTER TRAINING ROUND {i}: \n")
                 print(f"MSE OF THE TRAINED MODEL AFTER TRAINING ROUND {i}: ")
                 acc = test(data_loader_test, trained_model, loss_fn, device=global_device)
-                file.write(f"Loss: {acc}")
+                file.write(f"Loss: {acc}\n")
                 print(f"Loss: {acc}")
                 losses.append(acc)
                 if acc < min_acc:
@@ -70,13 +70,13 @@ if __name__ == "__main__":
                 train(data_loader_train, minimum_model, loss_fn, minimum_optim, global_device)
                 acc_min = test(data_loader_test, minimum_model, loss_fn, device=global_device)
                 file.write(f"MSE Loss for One Pass + Gradient: {acc_min}\n")
-                print(f"MSE Loss for One Pass + Gradient: {acc_min}\n")
+                print(f"MSE Loss for One Pass + Gradient: {acc_min}")
                 train(data_loader_train, model_copy, loss_fn, copy_optim, global_device)
                 acc_copy = test(data_loader_test, model_copy, loss_fn, device=global_device)
-                file.write(f"MSE Loss for Gradient: {acc_copy}")
+                file.write(f"MSE Loss for Gradient: {acc_copy}\n")
                 print(f"MSE Loss for Gradient: {acc_copy}")
-            save_model_parameters(minimum_model, model_string, f'one_pass_grad_round_{j}', 'D:\\pass_gradient_exp\\regression_test_2', global_device)
-            save_model_parameters(model_copy, model_string, f'just_grad_round_{j}', 'D:\\pass_gradient_exp\\regression_test_2', global_device)
+            save_model_parameters(minimum_model, model_string, f'one_pass_grad_round_{j}', 'D:\\pass_gradient_exp\\regression_test_3', global_device)
+            save_model_parameters(model_copy, model_string, f'just_grad_round_{j}', 'D:\\pass_gradient_exp\\regression_test_3', global_device)
             min_loss = test(data_loader_test, minimum_model, loss_fn, global_device)
             trained_min_loss.append(min_loss)
             copy_loss = test(data_loader_test, model_copy, loss_fn, device=global_device)
@@ -88,6 +88,6 @@ if __name__ == "__main__":
         gathered_data = pd.DataFrame({"pass_grad": trained_min_loss, "grad": trained_copy_loss, "percent": percent_improvements})
         gathered_data["differences"] = gathered_data["grad"] - gathered_data["pass_grad"] 
         sns.histplot(gathered_data, x="percent", kde=True).set_title("Percent Improvements")
-        gathered_data.to_csv("D:\\pass_gradient_exp\\regression_test_2\\stats.csv", index=False)
+        gathered_data.to_csv("D:\\pass_gradient_exp\\regression_test_3\\stats.csv", index=False)
         plt.show()
         
