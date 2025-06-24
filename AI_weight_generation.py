3ef482b0c8eb5b8754bd50b38cbb36ea91990964
Jacob Maurer <jacob.maurer.24@cnu.edu>
2025-06-24 18:44:10 -0400

    modifying run_tests to better accomidate certain inputs (Data can now be a tuple of dataloaders, and a base model can be set instead of random model generation)

--- AI_weight_generation.py
+++ AI_weight_generation.py
@@ -34,15 +34,21 @@ class GeneratedDataset(Dataset):
     def __getitem__(self, index):
         return self.x[index], np.float32(self.y[index])
 
-def run_tests(data, device, num_test, epochs, min_error, error_func, optimize, activation_pool, insert_path):
-    data_set_train = GeneratedDataset(data[0][int(len(data[0])*.2):], data[1][int(len(data[1])*.2):])
-    data_set_test = GeneratedDataset(data[0][:int(len(data[0])*.2)], data[1][:int(len(data[1])*.2)])
-    data_load_train = DataLoader(data_set_train, batch_size=50, shuffle=True)
-    data_load_test = DataLoader(data_set_test, batch_size=50, shuffle=True)
+def run_tests(data, device, num_test, epochs, min_error, error_func, optimize, activation_pool, insert_path, base_model = None):
+    data_load_train, data_load_test = None, None
+    if isinstance(data, np.ndarray):    
+        data_set_train = GeneratedDataset(data[0][int(len(data[0])*.2):], data[1][int(len(data[1])*.2):])
+        data_set_test = GeneratedDataset(data[0][:int(len(data[0])*.2)], data[1][:int(len(data[1])*.2)])
+        data_load_train = DataLoader(data_set_train, batch_size=50, shuffle=True)
+        data_load_test = DataLoader(data_set_test, batch_size=50, shuffle=True)
+    if isinstance(data, tuple):
+        data_load_train = data[0]
+        data_load_test = data[1]
     lr = 1e-4
     for i in range(num_test):
-        model_str = model_string_generator(100, random.choice([0,1,2]), 1, list(activation_pool.keys()), (40, 450))
-        model = NetworkSkeleton(create_layers(model_str, activation_pool))
+        if base_model == None:
+            model_str = model_string_generator(100, random.choice([0,1,2]), 1, list(activation_pool.keys()), (40, 450))
+            model = NetworkSkeleton(create_layers(model_str, activation_pool))
         save_model_parameters(model, model_str, insert_path, 'pre', device)
         j = 0
         acc = min_error + 1
@@ -51,7 +57,7 @@ def run_tests(data, device, num_test, epochs, min_error, error_func, optimize, a
             acc = test(data_load_test, model, error_func, device)
             j += 1
         save_model_parameters(model, model_str, insert_path, 'post', device)
-        with open(path.join("regression", "reports", model_str_file_name(model_str) + "_report.txt"), 'wt') as file:
+        with open(path.join(insert_path, model_str_file_name(model_str) + "_report.txt"), 'wt') as file:
             file.write(f"Model String: {model_str}\nModel Accuracy: {acc}\nEpochs Ran: {j}\nOptimizer: {optimize}\nLearning Rate: {1e-4}\nError Function: {error_func}")
             
 if __name__ == "__main__":
