be09e971c30fe517ed3974e4e8f5f29d518d309c
Jacob Maurer <jacob.maurer.24@cnu.edu>
2025-07-01 11:13:15 -0400

    fixed the gather_model_paths function, starting on loading the data into memeroy via MemModel class.

--- heatmap_analysis.py
+++ heatmap_analysis.py
@@ -49,7 +49,7 @@ def open_log(base_path, layer_files):
         report_obj: Report = Report(lines[0].strip().split(":")[-1].strip(), float(lines[1].strip().split(":")[-1]), int(lines[2].strip().split(":")[-1]), lines[3].strip().split(":")[-1].strip(), float(lines[4].strip().split(":")[-1]), lines[5].strip().split(":")[-1].strip())
         return report_obj
 
-def gather_dataset_paths(fpath):
+def step_file_system(fpath):
     model_paths = []
     for root, dirs, files in os.walk(fpath):
         for dir in dirs:
@@ -57,51 +57,40 @@ def gather_dataset_paths(fpath):
         break
     return model_paths
 
-def gather_data(class_path, regress_path):
-    model_dirs_regress = gather_dataset_paths(gather_dataset_paths(regress_path))
-    model_dirs_class = gather_dataset_paths(gather_dataset_paths(class_path))
-    for model_dir in model_dirs_regress:
-        pass
-        
+def gather_model_paths(class_path, regress_path):
+    data_dirs_regress = step_file_system(regress_path)
+    data_dirs_class =  step_file_system(class_path)
+    model_dirs_regress = []
+    model_dirs_class = []
+    for index, model_dir in enumerate(data_dirs_regress):
+        model_dirs_regress.extend(step_file_system(model_dir))
+        model_dirs_class.extend(step_file_system(data_dirs_class[index]))
+    
+def load_references()
 
 if __name__ =='__main__':
-    # class_path = "D:\\model_dataset\\classification"
-    # regress_path = "D:\\model_dataset\\regression"
-    # model_path = "D:\\model_dataset\\classification\\2000_rows\\model_1"
-    # roots, directories, file_names = [], [], []
-    # for root, dirs, files in os.walk(model_path):
-    #     roots.append(root)
-    #     directories.append(dirs)
-    #     file_names.append(files)
-    #     break
-    # gather_data(class_path, regress_path)
-    # logs = open_log(model_path, file_names[0])
-    # print(logs)
-    # pre_train, post_train = sort_pre_post(file_names[0])
-    # pre_net = open_network(model_path, pre_train)
-    # post_net = open_network(model_path, post_train)
-    # fig, axs = plt.subplots(1,3)
-    # im1 = axs[0].imshow(pre_net[4].to_numpy())
-    # im2 = axs[1].imshow(post_net[4].to_numpy())
-    # im3 = axs[2].imshow((post_net[4].to_numpy() - pre_net[4].to_numpy()))
-    # fig.colorbar(im1, ax=axs[0])
-    # fig.colorbar(im2, ax=axs[1])
-    # fig.colorbar(im3, ax=axs[2])    
-    # plt.show()
-    
-    main_path = "D:\\pass_grad_alt_model\\regression_test_1\\weights"
-    fpaths = []
-    file_names = []
-    for root, dirs, files in os.walk(main_path):
-        for file in files:
-            file_names.append(file)
-            fpaths.append(main_path + "\\" + file)
+    class_path = "D:\\model_dataset\\classification"
+    regress_path = "D:\\model_dataset\\regression"
+    model_path = "D:\\model_dataset\\classification\\2000_rows\\model_1"
+    roots, directories, file_names = [], [], []
+    for root, dirs, files in os.walk(model_path):
+        roots.append(root)
+        directories.append(dirs)
+        file_names.append(files)
         break
-    for i, path in enumerate(fpaths):
-        weights = pd.read_csv(path)
-        plt.imshow(weights)
-        plt.title(file_names[i])
-        plt.colorbar()
-        plt.show()
+    gather_data(class_path, regress_path)
+    logs = open_log(model_path, file_names[0])
+    print(logs)
+    pre_train, post_train = sort_pre_post(file_names[0])
+    pre_net = open_network(model_path, pre_train)
+    post_net = open_network(model_path, post_train)
+    fig, axs = plt.subplots(1,3)
+    im1 = axs[0].imshow(pre_net[4].to_numpy())
+    im2 = axs[1].imshow(post_net[4].to_numpy())
+    im3 = axs[2].imshow((post_net[4].to_numpy() - pre_net[4].to_numpy()))
+    fig.colorbar(im1, ax=axs[0])
+    fig.colorbar(im2, ax=axs[1])
+    fig.colorbar(im3, ax=axs[2])    
+    plt.show()
     
     print("Hello World")
