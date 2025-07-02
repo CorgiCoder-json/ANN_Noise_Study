e7499de83fe44f0af522621980b4b4e160f0c018
Jacob Maurer <jacob.maurer.24@cnu.edu>
2025-07-02 10:49:08 -0400

    Starting the data analysis of each dataset

--- model_data_exploration.py
+++
@@ -1,8 +0,0 @@
-from heatmap_analysis import Report, gather_model_paths, load_references, open_log
-
-if __name__ == "__main__":
-    class_path = "D:\\model_dataset\\classification"
-    regress_path = "D:\\model_dataset\\regression"
-    model_paths = gather_model_paths(class_path, regress_path)
-    class_models = load_references(model_paths[1])
-    regress_models = load_references(model_paths[0])
